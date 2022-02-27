7011e707fe4df6a8e4c1c0efc6d658997ff2bc64
ttheland <tth00007@students.stir.ac.uk>
2022-02-27 12:29:13 +0000

    basic test cases for PersonController methods

--- Feedback.UnitTests/PersonsControllerTests.cs
+++ Feedback.UnitTests/PersonsControllerTests.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Feedback.Api.Controllers;
 using Feedback.Api.DTOs;
 using Feedback.Api.Entities;
 using Feedback.Api.Repositories;
 using FluentAssertions;
+using FluentAssertions.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
@@ -40,7 +42,7 @@ namespace Feedback.UnitTests
             result.Result.Should().BeOfType<NotFoundResult>();
         }
         [Fact]
-        public async Task GetPersonAsync_WithExistingId_ReturnsExpectedPerson()
+        public async Task GetPersonAsync_WithExistingPerson_ReturnsExpectedPerson()
         {
             // arrange
             var expectedPerson = CreateRandomPerson(); 
@@ -68,8 +70,107 @@ namespace Feedback.UnitTests
                 options => options.ComparingByMembers<Person>());
         }
 
+        [Fact]
+        public async Task GetPersonsAsync_WithExistingPersons_ReturnsAllPersons()
+        {
+            // arrange
 
-        // method to create new person with mock data
+            // create 3 random Persons
+            var expectedPersons = new[] { CreateRandomPerson(), CreateRandomPerson(), CreateRandomPerson() };
+
+            repositoryStub.Setup(repo => repo.GetPersonsAsync())
+                .ReturnsAsync(expectedPersons);
+            
+            var controller = new PersonsController(repositoryStub.Object, loggerStub.Object);
+
+            // act
+            var actualPersons = await controller.GetPersonsAsync();
+
+            // assert
+            actualPersons.Should().BeEquivalentTo(
+                expectedPersons,
+                options => options.ComparingByMembers<Person>());
+            
+        }
+
+        [Fact]
+        public async Task CreatePersonAsync_WithPersonToCreate_ReturnsCreatedPerson()
+        {
+            // arrange
+            var personToCreate = new CreatePersonDTO(){
+                Name = Guid.NewGuid().ToString(),
+                JobTitle = Guid.NewGuid().ToString(),
+            };
+            
+            var controller = new PersonsController(repositoryStub.Object, loggerStub.Object);
+
+            // act
+            var result = await controller.CreatePersonAsync(personToCreate); 
+
+            // assert
+            // retrieve personDTO
+            var createdPerson = (result.Result as CreatedAtActionResult).Value as PersonDTO;
+            personToCreate.Should().BeEquivalentTo(
+                createdPerson,
+                options => options.ComparingByMembers<PersonDTO>().ExcludingMissingMembers()    // only compare properties existing in both
+            );
+
+            createdPerson.Id.Should().NotBeEmpty();
+            createdPerson.CreatedDate.Should().BeCloseTo(DateTimeOffset.UtcNow, 10.Seconds());
+            
+        }
+
+        [Fact]
+        public async Task UpdatePersonAsync_WithExistingPerson_ReturnsNoContent()
+        {
+            // arrange
+             var existingPerson = CreateRandomPerson(); 
+
+            // return person with any guid
+            repositoryStub.Setup(repo => repo.getPersonAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(existingPerson);
+
+            var personId = existingPerson.Id;
+            var personToUpdate = new UpdatePersonDTO()
+            {
+                Name = Guid.NewGuid().ToString(),
+                JobTitle = Guid.NewGuid().ToString()
+            };
+            
+            var controller = new PersonsController(repositoryStub.Object, loggerStub.Object);
+
+            // act
+
+            var result = await controller.UpdatePersonAsync(personId, personToUpdate);
+
+            // assert
+            result.Should().BeOfType<NoContentResult>();
+            
+        }
+
+        [Fact]
+        public async Task DeletePersonAsync_WithExistingPerson_ReturnsNoContent()
+        {
+            // arrange
+             var existingPerson = CreateRandomPerson(); 
+
+            // return person with any guid
+            repositoryStub.Setup(repo => repo.getPersonAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(existingPerson);
+
+            
+            var controller = new PersonsController(repositoryStub.Object, loggerStub.Object);
+
+            // act
+            var result = await controller.DeletePersonAsync(existingPerson.Id);
+
+
+            // assert
+            result.Should().BeOfType<NoContentResult>();
+            
+        }
+
+        // method to create new Person with mock data
         private Person CreateRandomPerson()
         {
             return new()
@@ -77,6 +178,7 @@ namespace Feedback.UnitTests
                 Id = Guid.NewGuid(),
                 Name = Guid.NewGuid().ToString(),
                 JobTitle = Guid.NewGuid().ToString(),
+                Projects = null,
                 CreatedDate = DateTimeOffset.UtcNow
             };
         }
